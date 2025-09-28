58c8d5a9a63c88b6fcbf64625874283de7dac365
fre3ak <20868040+fre3ak@users.noreply.github.com>
2025-09-28 13:04:32 +0100

    feat: server cors update

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ['http://localhost:5173',
-           'https//:gofoodyyy.netlify.app'],
+  origin: ['https//:gofoodyyy.netlify.app',
+           'http://localhost:5173'],
   optionsSuccessStatus: 200
 };
 
